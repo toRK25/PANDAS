be6f521d07b45af61ad2d33d61dbffbfb8adcb4a
VEDANT SINGH <vedantsingh591@gmail.com>
2025-08-16 14:38:33 +0530

    Create panda.py

---
+++ SERIES/panda.py
@@ -0,0 +1,12 @@
+#a series is a one dimensional array-like object that can hold any data type
+import numpy as np
+import pandas as pd 
+
+label = ['a','b','c','d']
+list = [12,21,43,22]
+arr=np.array([3,5,2,4])
+dic={'a':2,'b':1,'c':3,'d':7}
+
+#creating a series from a list
+# print(pd.Series(list))
+print(pd.Series(list,index=label))
