4d5e96b59fab801227afdfaa34774ce6e54569de
VEDANT SINGH <vedantsingh591@gmail.com>
2025-08-17 12:43:21 +0530

    Update data.py

--- DATA FRAMES/data.py
+++ DATA FRAMES/data.py
@@ -1,7 +1,7 @@
 #creating a dataframe
 # SELECTION AND INDEXING OF COLUMNS
 # CREATING NEW COLUMNS
-# REMOVING COLUMNS
+# REMOVING COLUMNS 
 # SELECTING ROWS
 # SELECTING SUBSET OF ROWS AND COLUMNS
 # CONDITIONAL SELECTION
@@ -33,9 +33,22 @@ print("WITHOUT INDEXING \n",pd.DataFrame(list))
 columns = ["Name", "Age", "City", "Salary"]
 
 df = pd.DataFrame(list,columns=columns)
-print(df)
+print('WITH INDEXING\n',df)
 
 
 #SELECTION AND INDEXING OF COLUMNS
-print("SELECTING COLUMNS \n", df['Name'])  # Selecting a single column
+print(df['Name'])  # Selecting a single column
+print(df[['Name', 'City']])# Selecting multiple columns
+print('\n',df.Name)  # Another way to select a single column
+# CREATING NEW COLUMNS
+df['Experience'] = [5, 10, 3, 15]  # Adding a new column
+print('\n',df) 
+print(df.drop('Experience', axis=1 ,inplace =True))  # Removing a column permanently
+# print(df.drop('Experience', axis=1))  # Removing a column (note: use drop() method, not drop[])
+print('\n',df) # experience is still there if you need to remove it permanently use keyword INPLACE = True
 
+#for removing rows u just do axis = 0
+
+#rows
+
+print(df.loc[0])  # Selecting a single row by index
