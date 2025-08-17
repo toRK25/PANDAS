009335cbbd66e1a004a740c03f9e6e706b736b90
VEDANT SINGH <vedantsingh591@gmail.com>
2025-08-17 20:19:01 +0530

    Update missing.py

--- DATA FRAMES/missing.py
+++ DATA FRAMES/missing.py
@@ -25,9 +25,27 @@ print(df.isnull().mean() * 100)
 print(df.isnull().any())
 
 
-#fill missing values with a specific value
-df_filled = df.fillna(0)
-print("DataFrame after filling missing values with 0:", df_filled)
+
 #removing null value
 df_dropped = df.dropna()
 print("DataFrame after dropping rows with missing values:", df_dropped)
+
+#remove a particular column
+df_dropped_col = df.drop(columns=['B'])
+print("DataFrame after dropping column 'B':", df_dropped_col)
+
+dg= df.dropna(thresh=3)  # Drop rows with at least 2 non-NA values
+print("DataFrame after dropping rows with at least 2 non-NA values:", dg)
+
+# Selecting rows where COLUMN A is greater than 2
+print('Selecting rows where COLUMN A is greater than 2',df[df['A'] > 2])  # Conditional selection based on a column value
+
+
+#fill missing values with a specific value
+df_filled = df.fillna(0) #columnwise fill with 0
+print("DataFrame after filling missing values with 0:", df_filled)
+
+#fill with particular values
+value={'A': 0, 'B': 81, 'C': 22, 'D': 45}
+df_filled_specific = df.fillna(value)  # Fill with specific values for each column
+print("DataFrame after filling missing values with specific values:", df_filled_specific)
